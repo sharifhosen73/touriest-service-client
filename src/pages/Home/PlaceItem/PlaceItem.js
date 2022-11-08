27239e71669873096cfc6225c9306cb8d913c07a
Sharif <sharifhosen73@gmail.com>
2022-11-09 00:50:27 +0600

    react-photo-view

--- src/pages/Home/PlaceItem/PlaceItem.js
+++ src/pages/Home/PlaceItem/PlaceItem.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link, useLoaderData } from "react-router-dom";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 
 const PlaceItem = () => {
   const { name, img, description, price } = useLoaderData();
 
   return (
     <div className="card card-compact mx-auto w-3/4 bg-base-100 shadow-xl mt-8">
-      <figure>
-        <img className="h-96 w-full" src={img} alt="Shoes" />
-      </figure>
+      <PhotoProvider>
+        <PhotoView src={img}>
+          <img src={img} alt="" />
+        </PhotoView>
+      </PhotoProvider>
+
       <div className="card-body">
         <h2 className="card-title text-3xl">{name}</h2>
         <h2 className="card-title text-2xl">Cost: {price}</h2>
--- src/pages/Home/SinglePlace/SinglePlace.js
+++ src/pages/Home/SinglePlace/SinglePlace.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 
 const SinglePlace = ({ place }) => {
   const { _id, name, img, description } = place;
   return (
     <div className="card card-compact w-96 bg-base-100 shadow-xl mb-4">
-      <figure>
-        <img className="h-60 w-full" src={img} alt="Shoes" />
-      </figure>
+      <PhotoProvider>
+        <PhotoView src={img}>
+          <img src={img} className="h-60 w-full" alt="" />
+        </PhotoView>
+      </PhotoProvider>
+
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
         <div className="text-justify">
